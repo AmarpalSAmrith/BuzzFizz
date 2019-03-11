6c0a2729bf36c0befd0763e3754cd34f5a941ee1
Amarpal.Amrith <amarpal.amrith@AmarpalAmriths-MacBook-Pro.local>
2019-03-11 15:59:15 +0000

    streamed up

--- src/AmarpalAmrith/BuzzFizz/FizzBuzzBangStream.java
+++ src/AmarpalAmrith/BuzzFizz/FizzBuzzBangStream.java
@@ -1,9 +1,64 @@
 package AmarpalAmrith.BuzzFizz;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class FizzBuzzBangStream {
+
     public static Stream <String> create() {
-        
+        return Stream.iterate(1, n -> n + 1).map(FizzBuzzBangStream::convert);
+    }
+
+    private static String convert (int currentNumber) {
+
+        List<String> store = new ArrayList<>();
+
+        if (currentNumber % 3 == 0) {
+            store.add("Fizz");
+        }
+
+        if (currentNumber % 5 == 0) {
+            store.add("Buzz");
+        }
+
+        if (currentNumber % 7 == 0) {
+            store.add("Bang");
+        }
+
+        if (currentNumber % 11 == 0) {
+            store.clear();
+            store.add("Bong");
+        }
+
+        if (currentNumber % 13 == 0) {
+            int indexOfItemContainingB = indexOfItemWithSubString(store,"B");
+            if (indexOfItemContainingB != -1) {
+                store.add(indexOfItemContainingB, "Fezz");
+            } else {
+                store.add("Fezz");
+            }
+        }
+
+        if (currentNumber % 17 == 0) {
+            Collections.reverse(store);
+        }
+
+        if (store.size() == 0) {
+            return String.valueOf(currentNumber);
+        } else {
+            return String.join("", store);
+        }
+
+    }
+
+    private static int indexOfItemWithSubString(List <String> list, String criteria) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).contains(criteria)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
--- src/AmarpalAmrith/BuzzFizz/Main.java
+++ src/AmarpalAmrith/BuzzFizz/Main.java
@@ -1,88 +1,13 @@
 package AmarpalAmrith.BuzzFizz;
 
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
         Utilities.welcomeMsg("Hi, Welcome to FizzBuzz console!");
         int maxNumber = Utilities.requestInt("Please enter a maximum number to print to :)");
-
-        String customRules = Utilities.requestString("Would you like to only perform the default arguments or custom ones?");
-        int requestModulus = 0;
-        String requestRuleType = "";
-        String requestReplacementString = "";
-
-        if (customRules.equalsIgnoreCase("custom")) {
-            requestModulus = Utilities.requestInt("Please provide modulus");
-            requestRuleType = Utilities.requestString("Please provide what type of rule to be applied: Add Text, replace all text, reverse. ");
-            requestReplacementString = Utilities.requestString("Please provide string to replace number");
-
-        }
-
-        for (int i = 1; i <= maxNumber; i++) {
-
-            List <String> store = new ArrayList<>();
-
-            if (i % 3 == 0 && Arrays.asList(args).contains("3")) {
-                store.add("Fizz");
-            }
-
-            if (i % 5 == 0 && Arrays.asList(args).contains("5")) {
-                store.add("Buzz");
-            }
-
-            if (i % 7 == 0 && Arrays.asList(args).contains("7")) {
-                store.add("Bang");
-            }
-
-            if (i % 11 == 0 && Arrays.asList(args).contains("11")) {
-                store.clear();
-                store.add("Bong");
-            }
-
-            if (i % 13 == 0 && Arrays.asList(args).contains("13")) {
-                int indexOfItemContainingB = indexOfItemWithSubString(store,"B");
-                if (indexOfItemContainingB != -1) {
-                    store.add(indexOfItemContainingB, "Fezz");
-                } else {
-                    store.add("Fezz");
-                }
-            }
-
-            if (i % 17 == 0 && Arrays.asList(args).contains("17")) {
-                Collections.reverse(store);
-            }
-            if (i % requestModulus == 0) {
-                switch (requestRuleType.toLowerCase()) {
-                    case "add text":
-                        store.add(requestReplacementString);
-                        break;
-                    case "replace all text":
-                        store.clear();
-                        store.add(requestReplacementString);
-                        break;
-                    case "reverse":
-                        Collections.reverse(store);
-                        break;
-                }
-            }
-            if (store.size() == 0) {
-                System.out.println(i);
-            } else {
-                System.out.println(String.join("", store));
-            }
-
-        }
+        FizzBuzzBangStream.create().limit(maxNumber).forEach(System.out::println);
     }
 
-    private static int indexOfItemWithSubString(List <String> list, String criteria) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).contains(criteria)) {
-                return i;
-            }
-        }
-        return -1;
-    }
+
 
 }
